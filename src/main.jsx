697a6293552c28b525764026ac16f6ea5f519295
Andrés Steinshouer <asteinr10@gmail.com>
2025-09-17 00:05:25 -0600

    Se modificó el main para usar el router en vez de <App />

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import "./app/firebase";
+import "./app/firebase"; 
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import router from "./router/AppRouter"; 
 
-import router from "./app/router/AppRouter";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
